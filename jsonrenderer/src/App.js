571f22cdb5fe4b10be367ee761401c993b4bb379
udaypydi <udaypydi333@gmail.com>
2020-02-21 23:14:02 +0530

    feat(codeeditor): react code editor added

--- jsonrenderer/src/App.js
+++ jsonrenderer/src/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactJson from 'react-json-view';
-import logo from './logo.svg';
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
 import './App.css';
+
 
 let data = require('./ast.json');
 
 function App() {
+
+
   return (
     <div className="App">
       <h1>React Component AST</h1>
-      <ReactJson src={data} />
+      <div style={{ display: 'flex' }}>
+        <AceEditor
+          placeholder="Placeholder Text"
+          mode="javascript"
+          theme="monokai"
+          name="blah2"
+          onChange={(value) => console.log(value)}
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={`function onLoad(editor) {
+          console.log("i've loaded");
+        }`}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+        />
+        <ReactJson src={data} />
+      </div>
     </div>
   );
 }
