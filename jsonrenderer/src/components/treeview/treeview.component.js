55cb44c27a08454b43ed158ec1739698254554fd
udaypydi <udaypydi333@gmail.com>
2020-03-09 23:54:20 +0530

    sample tree view added

--- jsonrenderer/src/components/treeview/treeview.component.js
+++ jsonrenderer/src/components/treeview/treeview.component.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import Tree from 'react-d3-tree';
+ 
+const myTreeData = [
+  {
+    name: 'Top Level',
+    attributes: {
+      keyA: 'val A',
+      keyB: 'val B',
+      keyC: 'val C',
+    },
+    children: [
+      {
+        name: 'Level 2: A',
+        attributes: {
+          keyA: 'val A',
+          keyB: 'val B',
+          keyC: 'val C',
+        },
+      },
+      {
+        name: 'Level 2: B',
+      },
+    ],
+  },
+];
 
 const TreeView = (props) => {
     return (
         <div>
-            <p>Tree View</p>
+            <div id="treeWrapper" style={{width: '50em', height: '20em'}}>
+                <Tree data={myTreeData} />
+            </div>
         </div>
     )
 };
