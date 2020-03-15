97066f3329172d91a040e1d5551e05235bf2cc1e
udaypydi <udaypydi333@gmail.com>
2020-03-15 23:47:39 +0530

    fix eslint

--- jsonrenderer/src/components/treeview/treeview.component.js
+++ jsonrenderer/src/components/treeview/treeview.component.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Tree from 'react-d3-tree';
  import { myTreeData }  from './treeview.constant';
- 
+
 
 const TreeView = (props) => {
   const { ast } = props;
 
     return (
         <div>
-            <div id="treeWrapper" style={{width: '50em', height: '20em'}}>
+            <div 
+              id="treeWrapper" 
+              style={{
+                width: '50em', 
+                height: '20em'
+              }}
+            >
                 <Tree data={myTreeData} />
             </div>
         </div>
-    )
+    );
 };
 
 export default TreeView;
-
