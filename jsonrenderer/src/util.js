43838b24b79249f347fb14d97aa3b1a3ebeeef29
udaypydi <udaypydi333@gmail.com>
2020-03-20 23:05:22 +0530

    fix(ast generator):generate ast for tree rendering

--- jsonrenderer/src/util.js
+++ jsonrenderer/src/util.js
@@ -9,24 +9,49 @@
  *      end: Number,
  *      value: String,
  *  },
- * children: Array<Node>
+ *  children: Array<Node>
  * }
  */
 
-export const parseAST = (ast) => {
+ let treeData = {
+     children: [],
+ };
+
+
+export const parseAST = (ast, prevKey = '') => {
 
     // parse every key in ast to form a d3 tree json.
 
+    console.log(treeData);
     const astKeys = Object.keys(ast);
     if (Array.isArray(ast)) {
         ast.forEach(element => {
             parseAST(element);
         });
     } else {
+        if (prevKey === 'program' || prevKey === 'body') {
+            treeData.children.push({
+                name: ast.type,
+                attributes: {
+                    start: ast.start,
+                    end: ast.end,
+                }
+            });
+        } else {
+            treeData = {
+                name: ast.type,
+                attributes: {
+                    start: ast.start,
+                    end: ast.end,
+                },
+                children: treeData.children.length ? treeData.children : []
+            }
+        }
+    
         if (astKeys.indexOf("program") !== -1) {
-            parseAST(ast["program"]);
+            parseAST(ast["program"], 'program');
         } else if (astKeys.indexOf("body") !== -1) {
-            parseAST(ast["body"]);
+            parseAST(ast["body"], 'body');
         } else if (astKeys.indexOf("value") !== -1) {
             console.log(ast['value']);
         }
